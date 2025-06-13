e3d63ea8e74c8561438c98d4343580e26f12c047
jongar <jongarcicuevas@gmail.com>
2025-06-13 15:52:52 +0200

    Day 1 of 100

---
+++ 1_name_generator.py
@@ -0,0 +1,4 @@
+print("\tBienvenido al generador de nombres \n")
+nombre1=input("Introduzca ciudad de nacimiento: ")
+nombre2= input("Introduz animal favorito: ")
+print(nombre1 +" "+nombre2) #comment
