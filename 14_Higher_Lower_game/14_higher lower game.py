55ca8f5d691ca88ec417128add5c2ab95c03101b
jongar <jongarcicuevas@gmail.com>
2025-07-01 19:02:45 +0200

    Day 14 Higher or Lower game demo

---
+++ 14_Higher_Lower_game/14_higher lower game.py
@@ -0,0 +1,58 @@
+import random
+from game_data import logo, vs, data
+import os
+def clear():
+    os.system("cls" if os.name=="nt" else "clear")
+
+def print_d(d):
+    print(f"{d["name"]}, a {d["description"]} from {d["country"]}")
+
+def define_second_d(d1):
+    d2= random.choice(data)
+    while(d2==d1):
+        d2= random.choice(data)
+    return d2
+
+def ask_win(option, d1, d2):
+    if option=='A':
+        if d1["follower_count"]>= d2["follower_count"]:
+            return True
+        else:
+            return False
+    if option =='B':
+        if d1["follower_count"]> d2["follower_count"]:
+            return False
+        else:
+            return True
+
+score=0
+def main(d1): 
+    global score
+    print(logo)
+    if d1 is None:
+        score=0
+        d1= random.choice(data)
+    d2= define_second_d(d1)
+    print("option A",end=': ')    
+    print_d(d1)
+    print(vs)
+    print("Option B", end= ': ')
+    print_d(d2)
+    option= input("Introduce A or B: ").upper()
+    if ask_win(option, d1, d2):
+        score+=1
+        clear()
+        print(f"Good job! your actual score is {score}")
+        main(d2)
+
+    else:
+        clear()
+        print(f"you loose! with a score of {score}")
+        if (input("Play again 'y' or 'n': ").lower()=='y'):
+            clear()
+            main(None)
+        else:
+            return
+
+
+main(None)
